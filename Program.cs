b95a3d3b90fdb9ff83b37ad9a0bd72584733330e
luisantonioea <lantonioespinoza10@gmail.com>
2026-04-23 02:47:12 -0500

    fix: eliminar completamente WithStaticAssets para compatibilidad con .NET 8

--- Program.cs
+++ Program.cs
@@ -63,8 +63,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseStaticFiles(); 
 
